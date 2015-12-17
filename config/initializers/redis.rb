26a5a15cb5aea68232c1a3051cb9176ab1682c0f
h3poteto <h3.poteto@gmail.com>
2015-12-18 01:52:04 +0900

    redis clientを外側に出す

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
-require 'redis'
-
-RedisClient = Redis.new(host: "127.0.0.1", port: 6379)
+GuessPaging::RedisClient.setup do |config|
+  config.redis_port = 6379
+  config.redis_host = '127.0.0.1'
+end
