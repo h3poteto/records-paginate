b08893ff97d4f6986cbddb376fcd797f9a76c55c
h3poteto <h3.poteto@gmail.com>
2015-12-19 20:12:26 +0900

    sample用に不要なコードを除去

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,23 +1,20 @@
 class RecordsController < ApplicationController
   def index
     # 既存のkaminari
-    @records = Record.page(params[:page]).per(10)
+    @records = Record.order("updated_at DESC").page(params[:page]).per(10)
   end
 
   def search
-    # 新しいやり方を考える
-    # TODO: このオブジェクトを全部ARオブジェクトに載せられればいいのかなぁ
-    # helperでARオブジェクトからそれらを取り出せたら最高
     if params[:category_id]
       @guess = GuessPaging::Paginate.new(
-        query: Record.where(category_id: params[:category_id].to_i),
+        query: Record.where(category_id: params[:category_id].to_i).order('updated_at DESC'),
         per_page: 10)
       @guess.guess(
         page_params: params[:page]
       )
     else
       @guess = GuessPaging::Paginate.new(
-        query: Record,
+        query: Record.order("updated_at DESC"),
         per_page: 10)
       @guess.guess(
         page_params: params[:page]
